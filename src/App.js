c322d3d52b5e743bacf3a019da1150871b10f107
kebe96 <93620532+kebe96@users.noreply.github.com>
2023-08-03 11:33:33 +0200

    premier modification

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          modifier <code>src/App.js</code> enregistrer et recharger pour voir les changements.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Apprendre React
         </a>
       </header>
     </div>
